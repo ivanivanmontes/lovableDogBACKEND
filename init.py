8adc542676bc9fa693dcc82194ade46853299f3d
Ivan Montes <101290628+ivanivanmontes@users.noreply.github.com>
2024-04-11 15:03:29 +0200

    very simple DB connection

--- init.py
+++ init.py
@@ -1,5 +1,8 @@
-# from main import app
+import psycopg2
 from flask import Flask
 from flask_cors import CORS
 app = Flask("__main__")
 CORS(app)
+# url = "postgres://dttccrum:XsuiMdrtUPYmWoTmH3wu7EIJkpTJ_FzU@bubble.db.elephantsql.com/dttccrum"
+url = "postgresql://postgres:2046@localhost:5432/test"
+connection = psycopg2.connect(url)
--- routes/test.py
+++ routes/test.py
@@ -1,7 +1,36 @@
+import psycopg2
 from main import app
 from flask import jsonify, Response, request, session
 
+url = "postgresql://postgres:2046@localhost:5432/test"
+
 # Define your route
-@app.route('/')
-def test():
-    return 'hello world!'
+@app.route('/', methods=["GET"])
+def test_connection() -> Response:
+    return jsonify("connection successful", 200)
+
+# DB test
+@app.route('/dbtest')
+def test_db_connection():
+    try:
+        # Establish a connection to the database
+        conn = psycopg2.connect(url)
+        
+        # Create a cursor to execute SQL queries
+        cur = conn.cursor()
+        
+        # Execute a sample SQL query
+        cur.execute("SELECT version()")
+        
+        # Fetch the result of the query
+        db_version = cur.fetchone()[0]
+        
+        # Close the cursor and connection
+        cur.close()
+        conn.close()
+        
+        return f"Connected to PostgreSQL! Database version: {db_version}"
+    
+    except psycopg2.Error as e:
+        # Handle any errors that occur during database connection
+        return f"Error connecting to PostgreSQL: {e}"
