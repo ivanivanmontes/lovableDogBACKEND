ee89947852708faa3a0fbfcf5ee1d9a8fefecb4e
Ivan Montes <101290628+ivanivanmontes@users.noreply.github.com>
2024-04-15 11:40:42 +0200

    add addPins route

--- routes/test.py
+++ routes/test.py
@@ -22,7 +22,6 @@ def test_db_connection():
         return f"Connected to PostgreSQL! Database version: {db_version}"
     
     except psycopg2.Error as e:
-        # Handle any errors that occur during database connection
         return f"Error connecting to PostgreSQL: {e}"
     
 
@@ -38,17 +37,29 @@ def add_pin() -> Response:
     description TEXT
 );
     """
-    data = request.get_json();
-    pin_id = data.get("Pin_ID");
-    latitude = data.get("latitude");
-    longitude = data.get("longitude");
-    title = data.get("title");
-    date_added = data.get("date_added");
-    desc = data.get("description")
+    try:
+        data = request.get_json();
+        pin_id = data.get("Pin_ID");
+        latitude = data.get("latitude");
+        longitude = data.get("longitude");
+        title = data.get("title");
+        date_added = data.get("date_added");
+        desc = data.get("description")
+        query = """
+            INSERT INTO pins (Pin_ID, latitude, longitude, title, date_added, description)
+            VALUES (%s, %s, %s, %s, %s, %s)
+        """
 
+        cur = connection.cursor()
+        cur.execute(query, (pin_id, latitude, longitude, title, date_added, desc))  # Passing values as a tuple
+        connection.commit();
+        cur.close();
 
-
-    ...
+        return jsonify("success", 200);
+    except psycopg2.Error as e:
+        # Handling the exception and converting to a JSON-friendly format
+        error_message = {"message": "error", "details": str(e)}
+        return jsonify(error_message), 500
 
 
 @app.route("/getPins", methods = ["GET"])
@@ -57,11 +68,17 @@ def get_pins() -> Response:
     SELECT *
     FROM pins
     """
-
-    cur = connection.cursor()
-    cur.execute(query)
-    raw_data = cur.fetchall()
-    return jsonify("success" , raw_data);
+    try:
+        cur = connection.cursor()
+        cur.execute(query)
+        raw_data = cur.fetchall()
+        cur.close()
+        column_name = [column[0] for column in cur.description]
+        result = [dict(zip(column_name, row)) for row in raw_data]
+        return jsonify(result);
+    except psycopg2.Error as e:
+        error_message = {"message": "error", "details": str(e)}
+        return jsonify({"error" : str(e)});
 
 
     
